f3304765ba57b93409dbb8272de0f537c508304c
m-kensuke <murata@craid-inc.com>
2022-04-19 13:00:14 +0900

    all file edited

--- create_done.php
+++ create_done.php
@@ -7,37 +7,44 @@
 <body>
 
 <?php
-
-try
+class create_done
 {
-	$title = $_POST['title'];
-	$contents = $_POST['contents'];
-
-	$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
-	$contents = htmlspecialchars($contents, ENT_QUOTES, 'UTF-8');
-
-	$dsn = 'mysql:dbname=todo;host=localhost;charset=utf8';
-	$user = 'root';
-	$password ='';
-	$dbh = new PDO($dsn, $user, $password);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	$sql = 'INSERT INTO mst_todo(title,content,created_at,updated_at) VALUES (?,?,CURRENT_TIME,CURRENT_TIME)';
-	$stmt = $dbh->prepare($sql);
-	$data[] = $title;
-	$data[] = $contents;
-	$stmt->execute($data);
-	$dbh = null;
-
-	echo $title;
-	echo 'を追加しました。<br />';
-
+	function CreateDone()
+	{
+		try
+		{
+			$title = $_POST['title'];
+			$contents = $_POST['contents'];
+	
+			$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+			$contents = htmlspecialchars($contents, ENT_QUOTES, 'UTF-8');
+	
+			$dsn = 'mysql:dbname=todo;host=localhost;charset=utf8';
+			$user = 'root';
+			$password ='';
+			$dbh = new PDO($dsn, $user, $password);
+			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+			$sql = 'INSERT INTO mst_todo(title,content,created_at,updated_at) VALUES (?,?,CURRENT_TIME,CURRENT_TIME)';
+			$stmt = $dbh->prepare($sql);
+			$data[] = $title;
+			$data[] = $contents;
+			$stmt->execute($data);
+			$dbh = null;
+	
+			echo $title;
+			echo 'を追加しました。<br />';
+	
+		}
+		catch (Exception $e)
+		{
+			echo 'ただいま故障により大変ご迷惑をおかけしております。';
+			exit();
+		}
+	}
 }
-catch (Exception $e)
-{
-	echo 'ただいま故障により大変ご迷惑をおかけしております。';
-	exit();
-}
+$create_done = new create_done();
+$create_done->CreateDone();
 ?>
 
 <a href="index.php">戻る</a>
